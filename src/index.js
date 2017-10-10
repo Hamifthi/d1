0b3ac0b297b864dc08c026266fb815639dc16566
Hamifthi <hamedonil@yahoo.com>
2017-10-10 18:42:53 +0330

    Primary codes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
-class D1 {
-    
+let D0 = require('E:/Hamed/Projects/Javascript/d1/lib/index');
+
+class D1{
+    constructor (point1, point2) {
+        this.equation = `${this.m} * ${x} + ${b}`
+        this.lineProperties = []
+    }
+
+    static getLineByEquation (m, b) {
+        const p1 = null;
+        const p2 = null;
+        return new D1(p1, p2)
+    }
+
+    getY (x) {
+        return eval(this.equation)
+    }
+
+    delta (point1, point2) {
+        if (point1 && point2 instanceof D0){
+            this.deltaList = []
+            const maxLength = math.max(point1.count(), point1.count())
+            for (let i = 0; i <= maxLength; i++) deltaList.push(point1.d(i) - point2.d(i))
+            return this.deltaList
+        }
+    }
+
+    get deltaY () {
+        return point1.d(1) - point2.d(1)
+    }
+
+    slope () {
+        this.m = this.deltaList[0] / this.deltaList[1]
+        return this.m
+    }
+
+    /*yIntercept (point1, point2) {
+        this.b = 
+    }*/
+
 }
 
 module.exports = D1
--- test/index.js
+++ test/index.js
@@ -1,2 +1,9 @@
-var package = require('../lib/index')
+const D1 = require('../lib/index')
+const D0 = require('../../d0/src/index')
 
+//const myLine = D1.getLineByEquation(2, 0)
+p1 = new D0(1, 2, 3)
+p2 = new D0(4, 5, 6)
+const myLine2 = new D1(p1, p2)
+
+console.log(myLine2.deltaYs)
